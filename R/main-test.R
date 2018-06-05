15841800e9610947afc5cdb9bc91e7a6a48c4f5c
Peter M C Harrison <pmc.harrison@gmail.com>
2018-06-05 12:54:19 +0100

    Adding admin panel

--- R/main-test.R
+++ R/main-test.R
@@ -17,14 +17,51 @@ show_item <- function(media_dir) {
       label = paste0("q", item_number),
       prompt = get_prompt(item_number, num_items_in_test),
       choices = get_choices(),
-      url = get_item_path(item, media_dir)
+      url = get_item_path(item, media_dir),
+      admin_ui = get_admin_ui(item, media_dir)
     )
   }
 }
 
+get_admin_ui <- function(item, media_dir) {
+  item$URL <- file.path(media_dir, item$Filename)
+  df <- item[, c("answer",
+                 "difficulty",
+                 "HeardRange",
+                 "Level",
+                 "AbsDiff_TrueIm_Probe",
+                 "Actual_LastHrd_probeprob",
+                 "URL")]
+  df$answer <- plyr::mapvalues(df$answer,
+                               from = c(0, 1),
+                               to = c("No match", "Match"),
+                               warn_missing = FALSE)
+  names(df) <- plyr::revalue(
+    names(df),
+    c(
+      answer = "Correct answer",
+      difficulty = "Difficulty",
+      HeardRange = "Number of unique played notes",
+      Level = "Number of imagined arrows",
+      AbsDiff_TrueIm_Probe = "Distance between true note and probe",
+      Actual_LastHrd_probeprob = "Probe probability based on distance"
+    ))
+  tab <- htmltools::tags$table(
+    lapply(seq_along(df),
+           function(i) shiny::tags$tr(
+             shiny::tags$td(names(df)[i],
+                            style = "padding:10px;"),
+             shiny::tags$td(format(df[[i]], digits = 3),
+                            style = "padding:10px;"))))
+  shiny::wellPanel(
+    shiny::h4("Item information"),
+    tab
+  )
+}
+
 get_item_path <- function(item, media_dir) {
   stopifnot(is(item, "item"), nrow(item) == 1L)
-  file.path(media_dir, paste0(item$Filename, ".mp4"))
+  file.path(media_dir, item$Filename)
 }
 
 get_prompt <- function(item_number, num_items_in_test) {
