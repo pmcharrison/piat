b2137a2350764c0e335c557cbb5a8fc9aadd74e1
Peter M C Harrison <pmc.harrison@gmail.com>
2018-06-05 18:28:45 +0100

    Reverting to Urry's criterion

--- R/options.R
+++ R/options.R
@@ -1,6 +1,6 @@
 piat.options <- function() {
   psychTestCAT::adapt_test_options(
-    next_item.criterion = "MFI",
+    next_item.criterion = "bOpt",
     next_item.estimator = "BM",
     next_item.prior_dist = "Jeffreys",
     # next_item.prior_par = c(0, 2),
