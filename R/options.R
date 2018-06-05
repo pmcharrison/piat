90eea5a17e720a1d1dc20822cbb69c3a5c0dc7cb
Peter M C Harrison <pmc.harrison@gmail.com>
2018-06-05 18:38:56 +0100

    New CAT params

--- R/options.R
+++ R/options.R
@@ -1,9 +1,9 @@
 piat.options <- function() {
   psychTestCAT::adapt_test_options(
     next_item.criterion = "bOpt",
-    next_item.estimator = "BM",
-    next_item.prior_dist = "norm",
-    next_item.prior_par = c(0, 2),
+    next_item.estimator = "WL",
+    # next_item.prior_dist = "norm",
+    # next_item.prior_par = c(0, 2),
     final_ability.estimator = "WL"
   )
 }
