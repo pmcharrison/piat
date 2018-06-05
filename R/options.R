4e7e1fbf00fe3089d310710b7414b889e8cda894
Peter M C Harrison <pmc.harrison@gmail.com>
2018-06-05 18:32:27 +0100

    back to normal but with wider prior

--- R/options.R
+++ R/options.R
@@ -2,8 +2,8 @@ piat.options <- function() {
   psychTestCAT::adapt_test_options(
     next_item.criterion = "bOpt",
     next_item.estimator = "BM",
-    next_item.prior_dist = "Jeffreys",
-    # next_item.prior_par = c(0, 2),
+    next_item.prior_dist = "norm",
+    next_item.prior_par = c(0, 2),
     final_ability.estimator = "WL"
   )
 }
