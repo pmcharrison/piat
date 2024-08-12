edd86d08af4d635a9a7395cca314924564ee6d6e
Peter Harrison <pmch2@cam.ac.uk>
2024-08-12 12:24:47 +0200

    Simplify test using Waldo

--- data-raw/piat-dict.R
+++ data-raw/piat-dict.R
@@ -1,3 +1,5 @@
+library(waldo)
+
 input_path <- "data-raw/piat-dict.csv"
 input <- read.csv(input_path, stringsAsFactors = FALSE, encoding = "utf-8")
 
@@ -5,24 +7,24 @@ input <- read.csv(input_path, stringsAsFactors = FALSE, encoding = "utf-8")
 archive_versions_to_check <- c("data-raw/piat-dict-archive-2.csv")
 for (path in archive_versions_to_check) {
   archive_version <- read.csv(path, stringsAsFactors = FALSE, encoding = "utf-8")
-  for (col in names(archive_version)) {
-    archive_values <- archive_version[[col]]
-    new_values <- input[[col]]
-    test <- all.equal(archive_values, new_values)
-    if (!isTRUE(test)) {
-      stop(
-        "Mismatch between ", input_path,
-        " and ", path, " in the ", col, " column. Details: ",
-        test
-      )
-    }
+
+  test <-
+    waldo::compare(
+      archive_version,
+      input[names(archive_version)]
+    )
+
+  if (length(test) > 0) {
+    print(test)
+    stop("Inconsistency found between ", input_path, " and ", path,
+         ", see above for details.")
   }
 }
 
 latvian <- read.csv("data-raw/dict-latvian.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(latvian)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
-          all(iput$key == latvian$key))
+          all(input$key == latvian$key))
 input$LV <- latvian$LV
 
 piat_dict <- psychTestR::i18n_dict$new(input)
